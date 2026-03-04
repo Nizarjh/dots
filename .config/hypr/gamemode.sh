2711fa84ee38d3514dcd912a10f444f9083a000a
Nizarjh <chel773@tutamail.com>
2026-03-05 01:23:27 +0400

    add waybar shutdown

--- .config/hypr/gamemode.sh
+++ .config/hypr/gamemode.sh
@@ -3,12 +3,17 @@ HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
+        keyword animation borderangle,0; \
+        keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
+	    keyword decoration:fullscreen_opacity 1;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
+    killall cava 2>/dev/null
+    killall waybar 2>/dev/null
     exit
 fi
 hyprctl reload
+waybar
