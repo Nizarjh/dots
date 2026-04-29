03dc1c10f4dd34aee59de4c019c598b16db26e6b
Nizarjh <chel773@tutamail.com>
2026-04-30 00:48:51 +0400

    refactoring my hyprConfig

--- .config/hypr/monitor_mode.sh
+++ .config/hypr/monitor_mode.sh
@@ -7,11 +7,11 @@ INTERNAL="eDP-1"
 
 # Check if we are currently mirroring
 if hyprctl monitors | grep -q "mirroring"; then
-  # Switch to Extended Mode
-  hyprctl keyword monitor "$EXTERNAL, preferred, auto, 1"
-  notify-send "Monitor Mode" "Switched to Extended Display"
+    # Switch to Extended Mode
+    hyprctl keyword monitor "$EXTERNAL, preferred, auto, 1"
+    notify-send "Monitor Mode" "Switched to Extended Display"
 else
-  # Switch to Mirror Mode
-  hyprctl keyword monitor "$EXTERNAL, preferred, auto, 1, mirror, $INTERNAL"
-  notify-send "Monitor Mode" "Switched to Mirror Display"
+    # Switch to Mirror Mode
+    hyprctl keyword monitor "$EXTERNAL, preferred, auto, 1, mirror, $INTERNAL"
+    notify-send "Monitor Mode" "Switched to Mirror Display"
 fi
